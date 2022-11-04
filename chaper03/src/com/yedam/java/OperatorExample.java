c852bf5828617c409c6191d4fbdfc574c48ebc50
eunjin <kej930713@gmail.com>
2022-11-04 11:56:59 +0900

    new

---
+++ chaper03/src/com/yedam/java/OperatorExample.java
@@ -0,0 +1,23 @@
+package com.yedam.java;
+
+public class OperatorExample {
+
+	public static void main(String[] args) {
+		//부호연산자
+		int a = -100;
+		int result1 = +a;
+		int result2 = -a;
+		System.out.println("result1 :" + result1);
+		System.out.println("result2 :" + result2);
+
+		byte b = 100;
+		//byte result3 = (byte)-b;
+		int result3 = -b;
+		System.out.println("result3 :" + result3);
+		
+		
+		
+
+	}
+
+}
